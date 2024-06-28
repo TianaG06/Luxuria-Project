98f3f78feedde5b442e41a315237094dab5bef7c
Holguer Pinzon <93726097+Hiceklent@users.noreply.github.com>
2024-06-28 00:20:04 -0500

    tercera entrega terminada

--- src/scripts/productListing.js
+++ src/scripts/productListing.js
@@ -1,11 +1,12 @@
 import { productData } from "/src/modules/data/products-data.js";
-
+const carShop = [];
+console.log(carShop + "este es mi carro");
 const productsArr = productData;
 
 // filtrado por tipo
 
 const productsFilterByType = (products, productType) => {
-    return products.filter((product) =>  product.type.toLowerCase() === productType.toLowerCase());
+    return products.filter((product) => product.type.toLowerCase() === productType.toLowerCase());
 };
 
 const productTypeCategorys = "Collar"
@@ -15,7 +16,7 @@ console.log(productosFiltrados);
 // filtrado por nombre
 
 const productsFilterByName = (products, TermOfSearch) => {
-        return products.filter((product) => product.name.toLowerCase().includes(TermOfSearch.toLowerCase()));
+    return products.filter((product) => product.name.toLowerCase().includes(TermOfSearch.toLowerCase()));
 };
 
 const NameSearchInput = "Lux";
@@ -26,46 +27,66 @@ console.log(productsFound);
 // Filtrado por precio
 
 const productsFilterByPrice = (products, order) => {
-
-    
     const orderedProducts = [...products];
     switch (order) {
-        case "acendente" : 
-        orderedProducts.sort((a, b) => a.price - b.price);
-        break;
-        case "decendente" : 
-        orderedProducts.sort((a, b) => b.price - a.price);
-        break
+        case "acendente":
+            orderedProducts.sort((a, b) => a.price - b.price);
+            break;
+        case "decendente":
+            orderedProducts.sort((a, b) => b.price - a.price);
+            break
         default:
-        break;
+            break;
 
     }
     return orderedProducts
 };
 
-console.log(productsFilterByPrice(productsArr,"acendente"));
-console.log(productsFilterByPrice(productsArr,"decendente"));
+console.log(productsFilterByPrice(productsArr, "acendente"));
+console.log(productsFilterByPrice(productsArr, "decendente"));
 
 
 // Total a pagar
 
+const cantidadProducto = 18;
+const productID = 4;
 
-const productosComprados = [
-    { name: "Luxury Charms Ring", count: 2, price: 620000 },
-    { name: "Dreamy Infinity Ring", count: 3, price: 327.71 },
-    { name: "Reflections Necklace", count: 1, price: 620.73 }
-];
+const AgregarAlCarrito = (id) => {
+    if (isNaN(productID) || isNaN(cantidadProducto) || cantidadProducto < 0) {
+        alert('ID no valido ');
+    } else {
+        const productSelected = productData.find(product => product.id === productID);
+        console.log(productSelected);
+        if (productSelected) {
+            const subtotal = productSelected.price * cantidadProducto;
 
+            carShop.push({
+                id: productSelected.id,
+                name: productSelected.name,
+                price: productSelected.price,
+                cantidad: cantidadProducto,
+                subtotal: subtotal
+            });
+            console.log(`Añadido al carrito: ${productSelected.name} - Cantidad: ${cantidadProducto}`);
+            console.log('Carrito:');
 
-const cantidadPedido = 5;
+            carShop.forEach(producto => {
+                console.log(`ID: ${producto.id}, Nombre: ${producto.name}, Cantidad: ${producto.cantidad}, Precio Unitario: ${producto.price}`);
+            });
+            calcularTotalCarrito();
+        } else {
+            alert('El producto seleccionado no existe.');
+        }
+    };
+}
 
-const totalToPay = (products, cantidad, price) => {
-    return products.reduce((total, products) => {
-        const cantidad = parseInt(products.count, 10);
-        const precioUnitario = parseInt(products.price, 10);
-        return total + (cantidad * precioUnitario);
-    }, 0);
-};
+const calcularTotalCarrito = () => {
+    const total = carShop.reduce((acu, item) => acu + item.subtotal, 0).toLocaleString('es-ES', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+        useGrouping: true,
+    });
+    console.log(`Total del carrito: $${total}`);
+}
 
-const totalToPayResult = totalToPay (productosComprados);
-console.log ("Total a pagar:", totalToPayResult);
+AgregarAlCarrito(productID);
