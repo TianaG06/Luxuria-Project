1785875ec11f091857ff05953c79af013b98d993
Holguer Pinzon <93726097+Hiceklent@users.noreply.github.com>
2024-07-06 10:54:20 -0500

    trabajando en product details

--- src/scripts/productDetails.js
+++ src/scripts/productDetails.js
@@ -2,13 +2,50 @@ import { productData } from "../modules/data/products-data.js";
 
 const urlParams = new URLSearchParams(window.location.search);
 const productId = parseInt(urlParams.get('id'));
+const product = productData.find(product => product.id === productId);
+
 const contenedorDeDetalles = document.getElementById('product-details-container');
 const productname = document.getElementById(productName);
 
+const contenedorDeBotonesColors =  document.getElementById('contenedorDeBotonesColors');
 
-function generarProductDetails(productId, listaDeProductos, contenedorDeDetalles) {
+const coloresDisponibles = [...new Set(product.inStock.map(product => product.color))];
+const tallasDisponibles = [...new Set(product.inStock.map(product => product.tallas))];
 
-    const product = listaDeProductos.find(product => product.id === productId);
+
+function agregarColoresDisponibles (coloresDisponibles,contenedorDeBotonesColors) {
+    coloresDisponibles.forEach(element => {
+        const item = document.createElement('li');
+        const boton = document.createElement("button");
+        boton.classList.add(element,'color-option-button');
+        boton.id = element;
+        
+        boton.addEventListener("click", () => {
+
+            console.log(`hola aqui ${element}`);
+            // ejecutarFiltros(element, contenedorDeBotonesColors.children, product)
+
+            activarBotonColores(contenedorDeBotonesColors.children, element);
+        });
+        item.appendChild(boton);
+        contenedorDeBotonesColors.appendChild(item);
+    });
+}
+
+
+function activarBotonColores(listaBotones, botonAActivar) {
+    for (const item of listaBotones) {
+        const boton = item.querySelector('button');
+        boton.classList.remove("colorSelected");
+        if (boton.id === botonAActivar) {
+            boton.classList.add("colorSelected");
+        }
+    }
+}
+
+
+function generarProductDetails(product, contenedorDeDetalles) {
+
 
     if (!product) {
         console.error(`El producto con ID ${productId} no se encuentra.`)
@@ -16,9 +53,21 @@ function generarProductDetails(productId, listaDeProductos, contenedorDeDetalles
     } else {
         const productname = document.getElementById('productName');
         productname.textContent = product.name;
-        // contenedorDeDetalles.innerHTML = "";
+
+        const productCode = document.getElementById('productCode');
+        productCode.textContent = product.bar_Code;
+
+        const productPrice = document.getElementById('productPrice');
+        productPrice.textContent = `$ ${(product.price).toLocaleString(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+
+        
+
+
+        
     }
 }
 
 
-generarProductDetails(productId, productData, contenedorDeDetalles);
+
+agregarColoresDisponibles(coloresDisponibles,contenedorDeBotonesColors);
+generarProductDetails(product, contenedorDeDetalles);
