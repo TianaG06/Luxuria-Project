4bc8c3fb332cb28450de0d9d0eaff8703da37615
Tatiana Garcia <t.dvla8549@gmail.com>
2024-07-11 11:15:54 -0500

    Agregando type al formulario y agregando constantes a formulario js

--- src/functions/formulario.js
+++ src/functions/formulario.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const checkoutForm = document.getElementById("checkoutForm");
     const nombreCompleto = document.querySelector("#nombre");
     const telefonoInput = checkoutForm.querySelector("#telefono");
+    const direccionInput = checkoutForm.querySelector("#direccion");
+    const emailInput = checkoutForm.querySelector("#email");
+    const nombreTarjetaInput = checkoutForm.querySelector("#card-name");
     const numeroTarjetaInput = checkoutForm.querySelector("#card-number");
     const expiryDateInput = checkoutForm.querySelector("#expiration-date");
     const numeroCvv = checkoutForm.querySelector("#cvv");
-    const emailInput = checkoutForm.querySelector("#email");
     const submitButton = checkoutForm.querySelector("button[type='submit']");
   
     submitButton.disabled = true;
